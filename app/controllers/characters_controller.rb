373bc7d5ce8c22335c2cfc8dfc6b6400c989dd72
hanaonomaru <hanao.n1227@gmail.com>
2021-06-10 01:17:31 +0900

    ブラウザに、キャラクター作成の送信フォームを作りました

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,5 +1,16 @@
 class CharactersController < ApplicationController
   def index
+    @character = AnimeCharacter.new
     @characters = AnimeCharacter.all
+  end
+
+  def create
+    AnimeCharacter.create!(character_params)
+    redirect_to action: :index
+  end
+
+  private
+  def character_params
+    params.require(:anime_character).permit(:name, :age, :gender, :voice_acter)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'characters#index'
+  post '/characters', to: 'characters#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
